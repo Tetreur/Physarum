03a6ab19fc2be62342b533c2b58a697b6293797c
Bastien <90622976+Tetreur@users.noreply.github.com>
2025-03-31 22:34:55 +0200

    format shit

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,20 +1,20 @@
 export function setupCanvas(canvas: HTMLCanvasElement) {
-  canvas.addEventListener("click", () => console.log(canvas));
-  const canvasContext = canvas.getContext("2d");
+	canvas.addEventListener('click', () => console.log(canvas))
+	const canvasContext = canvas.getContext('2d')
 
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+	canvas.width = window.innerWidth
+	canvas.height = window.innerHeight
 
-  // Redraw canvas on resize
-  window.addEventListener("resize", () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  });
+	// Redraw canvas on resize
+	window.addEventListener('resize', () => {
+		canvas.width = window.innerWidth
+		canvas.height = window.innerHeight
+	})
 
-  if (canvasContext === null) {
-    throw new Error("Canvas context is null");
-  }
+	if (canvasContext === null) {
+		throw new Error('Canvas context is null')
+	}
 
-  canvasContext.fillStyle = "red";
-  canvasContext.fillRect(50, 50, 100, 100);
+	canvasContext.fillStyle = 'red'
+	canvasContext.fillRect(50, 50, 100, 100)
 }
