93aaa043dd4cf578a0e644a51ac095e3a451041a
BastienGenin <132348029+BastienGenin@users.noreply.github.com>
2025-03-31 14:16:10 +0200

    feat (canvas.ts): Begin canvas logic implementation

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,4 +1,20 @@
-export function setupCanvas(element: HTMLCanvasElement) {
-	element.addEventListener('click', () => console.log('Bro'))
-	element.addEventListener('click', () => console.log('Bruuaauo'))
+export function setupCanvas(canvas: HTMLCanvasElement) {
+  canvas.addEventListener("click", () => console.log(canvas));
+  const canvasContext = canvas.getContext("2d");
+
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  // Redraw canvas on resize
+  window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  });
+
+  if (canvasContext === null) {
+    throw new Error("Canvas context is null");
+  }
+
+  canvasContext.fillStyle = "red";
+  canvasContext.fillRect(50, 50, 100, 100);
 }
