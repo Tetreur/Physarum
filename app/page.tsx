78205a80843a9e86cfb0edea7190939baea7d790
BastienGenin <132348029+BastienGenin@users.noreply.github.com>
2025-04-07 00:28:10 +0200

    Add Canvas component and update layout and page
    - Introduced Canvas component in Canvas.tsx
    - Updated metadata in layout.tsx
    - Removed Geist font and adjusted body class in layout.tsx
    - Replaced inline canvas with Canvas component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
+import { Canvas } from "./Canvas";
 
 export default function Home() {
   return (
-    <div className="flex grow justify-between flex-col items-end min-h-screen gap-2 font-[family-name:var(--font-geist-sans)] bg-black">
-      <main className="h-full w-full bg-gray-500">
-        <canvas className="h-full w-full" />
-      </main>
-      <footer className="w-full font-[family-name:var(--font-geist-mono)] p-2">
+    <div className="flex justify-between flex-col items-end min-h-screen gap-2 font-[family-name:var(--font-geist-mono)] bg-black">
+      <Canvas />
+      <footer className="w-full p-2">
         <a
           className="flex items-center gap-1 hover:underline hover:underline-offset-4 text-xs text-gray-500"
           href="https://cargocollective.com/sagejenson/physarum"
