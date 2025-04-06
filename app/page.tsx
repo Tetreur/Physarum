1ee3076de9d0c9d4ef50cb1b16a1ed3f26c326f6
BastienGenin <132348029+BastienGenin@users.noreply.github.com>
2025-04-07 00:23:31 +0200

    Update styles and links in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -2,28 +2,26 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex grow justify-between flex-col items-end min-h-screen px-7 pt-7 gap-2 font-[family-name:var(--font-geist-sans)] bg-black">
-      <main className="flex grow h-full w-full bg-gray-800 flex-col">
-        <canvas />
+    <div className="flex grow justify-between flex-col items-end min-h-screen gap-2 font-[family-name:var(--font-geist-sans)] bg-black">
+      <main className="h-full w-full bg-gray-500">
+        <canvas className="h-full w-full" />
       </main>
-      <footer>
-        <div className="text-xs text-gray-500 font-[family-name:var(--font-geist-mono)] pb-3">
-          <a
-            className="flex items-center gap-1 hover:underline hover:underline-offset-4"
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/brain.svg"
-              alt="brain icon"
-              width={16}
-              height={16}
-            />
-            Physarum
-          </a>
-        </div>
+      <footer className="w-full font-[family-name:var(--font-geist-mono)] p-2">
+        <a
+          className="flex items-center gap-1 hover:underline hover:underline-offset-4 text-xs text-gray-500"
+          href="https://cargocollective.com/sagejenson/physarum"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/brain.svg"
+            alt="brain icon"
+            width={16}
+            height={16}
+          />
+          Physarum
+        </a>
       </footer>
     </div>
   );
