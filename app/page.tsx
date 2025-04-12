b4ac5b98667a9d82e1a19cf97be1f5b752a791bd
BastienGenin <132348029+BastienGenin@users.noreply.github.com>
2025-04-12 16:18:47 +0200

    Create a drawer in its own component using vaul and lucide-react

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,15 @@
-import Image from "next/image";
-import { Canvas } from "./Canvas";
+'use client'
+
+import { Canvas } from './Canvas'
+import { DrawerComponent } from './components'
 
 export default function Home() {
-  return (
-    <div className="flex justify-between flex-col items-end min-h-screen gap-2 font-[family-name:var(--font-geist-mono)] bg-black">
-      <Canvas />
-      <footer className="w-full p-2">
-        <a
-          className="flex items-center gap-1 hover:underline hover:underline-offset-4 text-xs text-gray-500"
-          href="https://cargocollective.com/sagejenson/physarum"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/brain.svg"
-            alt="brain icon"
-            width={16}
-            height={16}
-          />
-          Physarum
-        </a>
-      </footer>
-    </div>
-  );
+	return (
+		<div className="flex justify-between flex-col items-end min-h-screen gap-2 font-[family-name:var(--font-geist-mono)] bg-black">
+			<Canvas />
+			<footer className="w-full p-2">
+				<DrawerComponent />
+			</footer>
+		</div>
+	)
 }
