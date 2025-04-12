7128badbe0f31cacd6ced3dd07c4877488d9b3d1
BastienGenin <132348029+BastienGenin@users.noreply.github.com>
2025-04-12 23:04:15 +0200

    Lint on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,25 @@
-import type { Metadata } from "next";
-import { Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Geist_Mono } from 'next/font/google'
+import './globals.css'
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: "Physarum",
-  description: "Physarum simulation",
-};
+	title: 'Physarum',
+	description: 'Physarum simulation',
+}
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${geistMono.variable} antialiased`}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${geistMono.variable} antialiased`}>{children}</body>
+		</html>
+	)
 }
