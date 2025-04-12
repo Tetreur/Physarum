7992ba90be7251038c52cbbac55fa36ba0cff3fe
BastienGenin <132348029+BastienGenin@users.noreply.github.com>
2025-04-12 23:03:57 +0200

    Update drawer

--- app/components/drawer.tsx
+++ app/components/drawer.tsx
@@ -1,4 +1,4 @@
-import { Info } from 'lucide-react'
+import { Brain, SquareArrowOutUpRight } from 'lucide-react'
 import { twMerge } from 'tailwind-merge'
 import { Drawer } from 'vaul'
 
@@ -7,86 +7,52 @@ export function DrawerComponent() {
 		<Drawer.Root>
 			<Drawer.Trigger
 				className={twMerge(
-					'ring',
-					'h-6 gap-2 px-2',
-					'shadow-sm rounded-full bg-white text-sm font-medium',
-					'relative flex flex-shrink-0 items-center gap-2 justify-center',
-					'transition-all hover:bg-[#FAFAFA] dark:bg-[#161615] dark:hover:bg-[#1A1A19] dark:text-white',
+					'cursor-pointer',
+					'absolute right-1/2 bottom-5 translate-x-1/2',
+					'rounded-full',
+					'text-sm font-mono text-neutral-500 hover:text-neutral-100',
+					'transition-all duration-300 ease-in-out',
 				)}
 			>
-				<Info size={16} />
-				more info
+				<Brain size={16} />
 			</Drawer.Trigger>
 			<Drawer.Portal>
-				<Drawer.Overlay className="fixed inset-0 bg-black/40" />
-				<Drawer.Content className="bg-gray-100 flex flex-col rounded-t-[10px] mt-24 h-fit fixed bottom-0 left-0 right-0 outline-none">
-					<div className="p-4 bg-white rounded-t-[10px] flex-1">
-						<div
-							aria-hidden
-							className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-gray-300 mb-8"
-						/>
-						<div className="max-w-md mx-auto">
-							<Drawer.Title className="font-medium mb-4 text-gray-900">
-								{' '}
-								Drawer for React.
-							</Drawer.Title>
-							<p className="text-gray-600 mb-2">
-								This component can be used as a Dialog replacement on mobile and
-								tablet devices.
-							</p>
-						</div>
+				<Drawer.Overlay className="fixed inset-0 bg-black/20" />
+				<Drawer.Content
+					className={twMerge(
+						'font-mono',
+						'bg-neutral-950 text-neutral-400',
+						'flex flex-col',
+						'rounded-xl overflow-clip',
+						'outline-none border border-neutral-900',
+						'mt-24 h-fit fixed bottom-4 left-0 right-0',
+						'w-1/2 right-1/2 translate-x-1/2',
+						'text-sm',
+					)}
+					style={
+						{
+							'--initial-transform': 'calc(100% + 16px)',
+						} as React.CSSProperties
+					}
+				>
+					<div className="p-6 bg-black">
+						<Drawer.Title className="mb-4 flex items-center gap-2 text-neutral-200">
+							<Brain color="#e5e5e5" size={16} strokeWidth={1.5} /> Physarum
+						</Drawer.Title>
+						Slime mold that can solve mazes and find the shortest path to food.
+						It is a fascinating organism that has been studied for its unique
+						behavior and ability to adapt to its environment.
 					</div>
-					<div className="p-4 bg-gray-100 border-t border-gray-200 mt-auto">
+					<div className="p-4 border-t border-gray-900 mt-auto">
 						<div className="flex gap-6 justify-end max-w-md mx-auto">
 							<a
-								className="text-xs text-gray-600 flex items-center gap-0.25"
-								href="https://github.com/emilkowalski/vaul"
+								className="text-xs flex items-center gap-1.5 hover:text-neutral-100"
+								href="https://github.com/Tetreur/Physarum"
 								target="_blank"
 								rel="noreferrer"
 							>
 								{' '}
-								GitHub{' '}
-								<svg
-									fill="none"
-									height="16"
-									stroke="currentColor"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									viewBox="0 0 24 24"
-									width="16"
-									aria-hidden="true"
-									className="w-3 h-3 ml-1"
-								>
-									<path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6" />
-									<path d="M15 3h6v6" />
-									<path d="M10 14L21 3" />
-								</svg>
-							</a>
-							<a
-								className="text-xs text-gray-600 flex items-center gap-0.25"
-								href="https://twitter.com/emilkowalski_"
-								target="_blank"
-								rel="noreferrer"
-							>
-								{' '}
-								Twitter{' '}
-								<svg
-									fill="none"
-									height="16"
-									stroke="currentColor"
-									strokeLinecap="round"
-									strokeLinejoin="round"
-									strokeWidth="2"
-									viewBox="0 0 24 24"
-									width="16"
-									aria-hidden="true"
-									className="w-3 h-3 ml-1"
-								>
-									<path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6" />
-									<path d="M15 3h6v6" />
-									<path d="M10 14L21 3" />
-								</svg>
+								GitHub <SquareArrowOutUpRight size={9} />
 							</a>
 						</div>
 					</div>
