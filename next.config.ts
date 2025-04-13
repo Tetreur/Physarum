b665f07eaf3969a9ca038124e6f6a36b48487509
BastienGenin <132348029+BastienGenin@users.noreply.github.com>
2025-04-13 20:27:34 +0200

    custom webpack loader for wgsl

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  webpack: (config) => {
+    config.module?.rules?.push({
+      test: /\.wgsl$/i,
+      use: "raw-loader",
+    });
+          
+  },
   experimental: {
     turbo: {
       rules: {
