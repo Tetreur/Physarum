49342552a0aa8ce9cc83a6564300950708616cc8
BastienGenin <132348029+BastienGenin@users.noreply.github.com>
2025-04-13 20:23:53 +0200

    Just ship it

---
+++ app/components/Drawer/Drawer.tsx
@@ -0,0 +1,71 @@
+import { Brain, Github } from 'lucide-react'
+import { twMerge } from 'tailwind-merge'
+import { Drawer } from 'vaul'
+import Trigger from './Trigger'
+
+export function DrawerComponent() {
+	return (
+		<div
+			className={twMerge(
+				'absolute w-full bottom-0 h-2/12',
+				'group',
+				'overflow-clip',
+			)}
+		>
+			<Drawer.Root>
+				<Trigger />
+				<Drawer.Portal>
+					<Drawer.Overlay className="fixed inset-0 bg-black/20" />
+					<Drawer.Content
+						className={twMerge(
+							'font-mono text-sm text-neutral-400',
+							'rounded-xs',
+							'bg-black',
+							'flex flex-col',
+							'overflow-clip',
+							'outline-none border border-neutral-900',
+							'mt-24 h-fit fixed bottom-4',
+							'w-1/2 right-1/2 translate-x-1/2',
+						)}
+						style={
+							{
+								'--initial-transform': 'calc(100% + 16px)',
+							} as React.CSSProperties
+						}
+					>
+						<div className="p-6 bg-black">
+							<Drawer.Title className="mb-4 flex items-center gap-2 text-neutral-200">
+								<Brain color="#e5e5e5" size={16} strokeWidth={1.5} /> Physarum
+							</Drawer.Title>
+							<Drawer.Description className="text-neutral-400">
+								Physarum polycephalum is a slime mold that can solve mazes and
+								find the shortest path to food. It is a fascinating organism
+								that has been studied for its unique behavior and ability to
+								adapt to its environment.
+							</Drawer.Description>
+						</div>
+
+						{/* <Engine Switches> */}
+						{/* <div className="p-6 border-t border-neutral-900 flex flex-col gap-4">
+							<Switch id="webgpu" />
+							<Switch id="webgl2" />
+						</div> */}
+
+						<div className="px-6 py-4 border-t border-neutral-900 bg-neutral-950 mt-auto">
+							<div className="flex gap-6 justify-end text-xs">
+								<a
+									className="text-xs flex items-center gap-1.5 hover:text-neutral-100"
+									href="https://github.com/Tetreur/Physarum"
+									target="_blank"
+									rel="noreferrer"
+								>
+									<Github size={14} />
+								</a>
+							</div>
+						</div>
+					</Drawer.Content>
+				</Drawer.Portal>
+			</Drawer.Root>
+		</div>
+	)
+}
---
+++ app/components/Drawer/Trigger.tsx
@@ -0,0 +1,119 @@
+import { type Scope, animate, createScope, svg, utils } from 'animejs'
+import { type SVGProps, useEffect, useRef } from 'react'
+import { twMerge } from 'tailwind-merge'
+import { Drawer } from 'vaul'
+
+function Trigger({
+	className,
+	...props
+}: React.ComponentPropsWithoutRef<typeof Drawer.Trigger>) {
+	const root = useRef<SVGSVGElement | null>(null)
+	const scope = useRef<Scope | null>(null)
+
+	useEffect(() => {
+		// A function to generate random points on #path-2 on each iteration
+		function generatePoints() {
+			const isOdd = (n: number) => n % 2
+
+			// Total number of point to generate on each iteration
+			const total = utils.random(5, 32)
+			const totalIteration = isOdd(total) ? total + 1 : total // Make sure the total is even
+
+			// Random radius for the first set of points
+			const r1 = utils.random(4, 56)
+
+			// Random radius for the second set of points
+			const r2 = 56
+
+			let points = ''
+			for (let i = 0; i < totalIteration; i++) {
+				const r = isOdd(i) ? r1 : r2
+				const a = (2 * Math.PI * i) / totalIteration - Math.PI / 2
+				const x = 152 + utils.round(r * Math.cos(a), 0)
+				const y = 56 + utils.round(r * Math.sin(a), 0)
+				points += `${x},${y} `
+			}
+			return points
+		}
+
+		const [$path1, $path2] = utils.$('polygon')
+
+		scope.current = createScope({
+			root: '.logo:hover',
+			// mediaQueries: { hover: '(hover: hover)' },
+		}).add((self) => {
+			function animateRandomPoints() {
+				// Update the points attribute on #path-2
+				utils.set($path2, { points: generatePoints() })
+				// Morph the points of #path-1 into #path-2
+				return animate($path1, {
+					points: svg.morphTo($path2),
+					ease: 'inOutCirc',
+					duration: 800,
+					autoplay: true,
+					onComplete: animateRandomPoints,
+				})
+			}
+
+			animateRandomPoints()
+		})
+
+		// Properly cleanup all anime.js instances declared inside the scope
+		return () => scope.current?.revert()
+	}, []) // Empty dependency array to run only once on mount
+
+	return (
+		<Drawer.Trigger
+			className={twMerge(
+				'cursor-pointer',
+				'absolute translate-x-1/2 right-1/2',
+				'-bottom-18',
+				'group-hover:bottom-5',
+				'border border-black hover:border-neutral-900',
+				'bg-black hover:bg-neutral-950',
+				'text-sm font-mono text-neutral-500',
+				'hover:text-neutral-300',
+				'transition-all duration-300 ease-in-out',
+				'rounded-xs',
+			)}
+		>
+			<Icon ref={root} size={60} className="logo" />
+		</Drawer.Trigger>
+	)
+}
+
+function Icon(
+	props: {
+		size?: number
+	} & SVGProps<SVGSVGElement>,
+) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width={props.size ?? '56'}
+			height={props.size ?? '56'}
+			viewTarget={'6'}
+			viewBox="0 0 304 112"
+			fill="none"
+			stroke="currentColor"
+			fillRule="evenodd"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			{...props}
+		>
+			<title>more info</title>
+			<polygon
+				className="stroke-1 hover:stroke-[30px]"
+				id="path-1"
+				points="152,4 170,38 204,56 170,74 152,108 134,74 100,56 134,38"
+			/>
+			<polygon
+				style={{ opacity: 0, strokeWidth: 12 }}
+				id="path-2"
+				points="152,4 170,38 204,56 170,74 152,108 134,74 100,56 134,38"
+			/>
+		</svg>
+	)
+}
+
+export default Trigger
